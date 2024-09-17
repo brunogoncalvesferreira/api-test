343347833b027252decb97b1eeecea3da339f06e
Bruno <74840490+brunogoncalvesferreira@users.noreply.github.com>
2024-09-16 22:45:13 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 export const routes = Router()
 
-routes.get('/', (req, res) => {
+routes.get('/hello', (req, res) => {
   return res.json({
     message: "Hello World"
   })
